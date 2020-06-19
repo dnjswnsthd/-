546da4ad7a368ec4e2083bed08901f4fd0457574
dnjswnsthd <dnjswnsthd@naver.com>
2020-06-19 19:32:50 +0900

    Add BOJ18258

---
+++ BOJ18258.py
@@ -0,0 +1,31 @@
+import sys
+from collections import deque
+n = int(sys.stdin.readline().rstrip())
+que = deque([])
+for i in range(n):
+    s = sys.stdin.readline().split()
+    if s[0] == "push":
+        que.append(s[1])
+    elif s[0] == "pop":
+        if not que:
+            print(-1)
+        else:
+            print(que.popleft())
+    elif s[0] =="front":
+        if not que:
+            print(-1)
+        else:
+            print(que[0])
+    elif s[0] == "back":
+        if not que:
+            print(-1)
+        else:
+            print(que[-1])
+    
+    elif s[0] == "size":
+        print(len(que))
+    elif s[0] == "empty":
+        if not que:
+            print(1)
+        else:
+            print(0)
