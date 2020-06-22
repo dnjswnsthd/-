34d756218661b0efa136e1f9dcd258d7d5152df9
dnjswnsthd <dnjswnsthd@naver.com>
2020-06-22 19:15:54 +0900

    Add BOJ15651

---
+++ BOJ15651.py
@@ -0,0 +1,31 @@
+import sys
+
+class Solution:
+
+    def __init__(self, n, m):
+        self.N = n
+        self.M = m
+        self.arr = [0 for _ in range(m)]
+        self.visited = [False for _ in range(n + 1)]
+
+    def backtracking(self, c, n, m):
+        if c == m:
+            for v in self.arr:
+                print(v, end=' ')
+            print()
+            return
+        for i in range(1, n + 1):
+            if self.visited[c] is False:
+                self.arr[c] = i
+                self.visited[c] = True
+                self.backtracking(c + 1, n, m)
+                self.visited[c] = False
+
+    def get_answer(self):
+        self.backtracking(0, self.N, self.M)
+
+
+if __name__ == "__main__":
+    N, M = map(int, sys.stdin.readline().rstrip().split())
+    s = Solution(N, M)
+    s.get_answer()
