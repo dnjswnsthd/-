39db7a160034e6ff2b0886c27752383ce6c30470
dnjswnsthd <dnjswnsthd@naver.com>
2020-06-26 18:43:52 +0900

    Add BOJ15656

---
+++ BOJ15656.py
@@ -0,0 +1,15 @@
+import sys
+
+def sol(arr, i):
+    if i == m:
+        print(' '.join(list(map(str, arr))))
+
+    else:
+        for j in data:
+            sol(arr+[j], i+1)
+
+n, m = map(int, sys.stdin.readline().rstrip().split())
+data = list(map(int, sys.stdin.readline().rstrip().split()))
+data.sort()
+
+sol([], 0)
