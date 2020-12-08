b65fa0e9907e2b550de5b5eade5854e81920063a
dnjswnsthd <dnjswnsthd@naver.com>
2020-12-08 21:07:01 +0900

    Add SWEA1289

---
+++ Algo/src/SWEA1289.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+ 
+public class SWEA1289 {
+ 
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+        int cnt;
+        char bit;
+        for (int t = 1; t <= tc; t++) {
+            String s = br.readLine();
+            bit = '0';
+            cnt = 0;
+            for (int i = 0; i < s.length(); i++) {
+                if(bit != s.charAt(i)) {
+                    cnt++;
+                    bit = s.charAt(i);
+                }
+            }
+            System.out.println("#" + t + " " + cnt);
+        }
+    }
+}
