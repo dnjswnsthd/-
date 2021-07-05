f920b8a192b11e848f06bddfce6ce50cf0e201ff
dnjswnsthd <dnjswnsthd@naver.com>
2021-07-05 15:01:19 +0900

    Add 프로그래머스 코딩테스트 연습 해쉬 전화번호 목록

--- Algo/src/PRO_HASH_1.java
+++ Algo/src/PRO_HASH_1.java
@@ -1,10 +1,11 @@
 import java.util.HashMap;
-import java.util.Map;
+
+//프로그래머스 코딩테스트 연습 해쉬 완주하지 못한 선수 
 
 class PRO_HASH_1 {
 	public String solution(String[] participant, String[] completion) {
         String answer = "";
-        Map <String, Integer> map = new HashMap<>();
+        HashMap <String, Integer> map = new HashMap<>();
         for(String par : participant){
             map.put(par, map.getOrDefault(par, 0) + 1);
         }
---
+++ Algo/src/PRO_HASH_2.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+
+// 프로그래머스 코딩테스트 연습 해쉬 전화번호 목록
+
+public class PRO_HASH_2 {
+	public static boolean solution(String[] phone_book) {
+		HashMap <String, Integer> map = new HashMap<>();
+        for(int i = 0; i < phone_book.length; i++){
+            map.put(phone_book[i], i);
+        }
+        for(int i = 0; i < phone_book.length; i++){
+            for(int j = 1; j < phone_book[i].length(); j++){
+                if(map.containsKey(phone_book[i].substring(0, j))){
+                        return false;
+                }
+            }
+        }
+            return true;
+    }
+	
+	
+//	public boolean solution(String[] phone_book) {
+//		for(int i = 0; i < phone_book.length-1; i++) {
+//			for(int j = i + 1; j < phone_book.length; j++) {
+//				if(phone_book[i].startsWith(phone_book[j]))return false;
+//				if(phone_book[j].startsWith(phone_book[i]))return false;
+//			}
+//		}
+//        return true;
+//    }
+}
