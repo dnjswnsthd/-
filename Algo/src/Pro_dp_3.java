5f7b1f10b575949480f5b4919967a001e566e7c3
dnjswnsthd <dnjswnsthd@naver.com>
2021-07-15 12:11:47 +0900

    Fix 프로그래머스 코딩테스트 연습 dp 등굣길

--- Algo/src/Pro_dp_3.java
+++ Algo/src/Pro_dp_3.java
@@ -8,30 +8,27 @@ public class Pro_dp_3 {
 		for(int i = 0; i < puddles.length; i++) {
 			dp[puddles[i][1]][puddles[i][0]] = -1;
 		}
-		for(int i = 1; i < dp.length; i++) {
-			for(int j = 1; j < dp[i].length; j++) {
+		for(int i = 1; i <= n; i++) {
+			for(int j = 1; j <= m; j++) {
 				if(dp[i][j] == -1) {
-					dp[i][j] = 0;
-				}else {
-					if(i == 1) {
-						dp[i][j] += dp[i][j-1];
-					}else {
-						dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % 1000000007;
-					}
+					continue;
 				}
-				if(j == dp[i].length - 1) {
-					answer = dp[i][j];
+				if(dp[i][j-1] >= 0 && dp[i][j] >= 0) {
+					dp[i][j] += dp[i][j-1] % 1000000007;
+				}
+				if(dp[i-1][j] >= 0 && dp[i][j] >= 0){
+					dp[i][j] += dp[i-1][j] % 1000000007;
 				}
 			}
 		}
+		answer = dp[n][m] % 1000000007;
 		
 		return answer;
 	}
-
 	public static void main(String[] args) {
 		int a = 4;
-		int b = 3;
-		int[][] c = { { 2, 2, } };
+		int b = 2;
+		int[][] c = {{2,2}};
 		System.out.println(solution(a, b, c));
 	}
 }
