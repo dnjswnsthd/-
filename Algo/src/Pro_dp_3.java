298f16f6d89c5ca1dc0cf1d819b9c91b74b0f1f5
dnjswnsthd <dnjswnsthd@naver.com>
2021-07-15 12:03:20 +0900

    Add 프로그래머스 코딩테스트 연습 dp 등굣길

---
+++ Algo/src/Pro_dp_3.java
@@ -0,0 +1,37 @@
+// 프로그래머스 코딩테스트 연습 dp 등굣길
+
+public class Pro_dp_3 {
+	public static int solution(int m, int n, int[][] puddles) {
+		int answer = 0;
+		int [][] dp = new int[n + 1][m + 1];
+		dp[1][1] = 1;
+		for(int i = 0; i < puddles.length; i++) {
+			dp[puddles[i][1]][puddles[i][0]] = -1;
+		}
+		for(int i = 1; i < dp.length; i++) {
+			for(int j = 1; j < dp[i].length; j++) {
+				if(dp[i][j] == -1) {
+					dp[i][j] = 0;
+				}else {
+					if(i == 1) {
+						dp[i][j] += dp[i][j-1];
+					}else {
+						dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % 1000000007;
+					}
+				}
+				if(j == dp[i].length - 1) {
+					answer = dp[i][j];
+				}
+			}
+		}
+		
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		int a = 4;
+		int b = 3;
+		int[][] c = { { 2, 2, } };
+		System.out.println(solution(a, b, c));
+	}
+}
