848a3a510798f418083744a5bdf1cca96a3f199a
dnjswnsthd <dnjswnsthd@naver.com>
2021-08-03 15:27:19 +0900

    Add 프로그래머스 내적

---
+++ Algo/src/Pro_inner.java
@@ -0,0 +1,16 @@
+// 프로그래머스 내적
+public class Pro_inner {
+    public static int solution(int[] a, int[] b) {
+        int answer = 0;
+        for (int i = 0; i < a.length; i++) {
+            answer += a[i] * b[i];
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] a = { 1, 2, 3, 4 };
+        int[] b = { -3, -1, 0, 2 };
+        System.out.println(solution(a, b));
+    }
+}
