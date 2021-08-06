7cf72ad24ca9c2c832a84d14268ce5fbe0f28856
dnjswnsthd <dnjswnsthd@naver.com>
2021-08-06 11:45:53 +0900

    A dd 프로그래머스 가운데 글자 가져오기

---
+++ Algo/src/Pro_centerWord.java
@@ -0,0 +1,18 @@
+// 프로그래머스 가운데 글자 가져오기
+public class Pro_centerWord {
+    public static String solution(String s) {
+        String answer = "";
+        if (s.length() % 2 == 0) {
+            answer += s.charAt((s.length() / 2) - 1);
+            answer += s.charAt((s.length() / 2));
+        } else {
+            answer += s.charAt((s.length() - 1) / 2);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        String s = "abcde";
+        System.out.println(solution(s));
+    }
+}
