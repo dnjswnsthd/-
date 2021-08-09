96675a7cd41e54ca1614e21c9250515a958deef8
dnjswnsthd <dnjswnsthd@naver.com>
2021-08-09 19:04:57 +0900

    Add 프로그래머스 코딩테스트 연습 직사각형 별찍기

---
+++ Algo/src/Pro_starSq.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+// 프로그래머스 코딩테스트 연습 직사각형 별찍기
+public class Pro_starSq {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        for (int i = 0; i < b; i++) {
+            for (int j = 0; j < a; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
