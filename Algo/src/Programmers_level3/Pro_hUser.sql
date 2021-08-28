9a70cfba624d1f92b7c596e978133259f061f855
dnjswnsthd <dnjswnsthd@naver.com>
2021-08-28 11:37:20 +0900

    Add 프로그래머스 코딩테스트 연습 헤비 유저가 소유한 장소

---
+++ Algo/src/Programmers_level3/Pro_hUser.sql
@@ -0,0 +1,10 @@
+-- 프로그래머스 코딩테스트 연습 헤비 유저가 소유한 장소
+SELECT ID, NAME, HOST_ID
+FROM PLACES
+WHERE HOST_ID IN(
+    SELECT HOST_ID
+    FROM PLACES
+    GROUP BY HOST_ID
+    HAVING COUNT(ID) >= 2
+)
+ORDER BY ID ASC;
