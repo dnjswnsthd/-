5189b7e76cfe395dadd06e493301cf2ffea2b93d
dnjswnsthd <dnjswnsthd@naver.com>
2021-09-26 15:29:48 +0900

    Fix 프로그래머스 코딩테스트 연습 야근 지수

--- Algo/src/Programmers_level3/Pro_work.java
+++ Algo/src/Programmers_level3/Pro_work.java
@@ -8,17 +8,23 @@ public class Pro_work {
     public static long solution(int n, int[] works) {
         long answer = 0;
         int sum = 0;
-        for (int i = 0; i < works.length; i++)
+        for(int i=0; i<works.length; i++)
             sum += works[i];
-        if (n >= sum)
+        if(n >= sum) 
             return 0;
-        while (n != 0) {
-            Arrays.sort(works);
-            works[works.length - 1]--;
-            n--;
+        while(n != 0) {
+            int max = 0;
+            for(int i=0; i<works.length; i++) 
+                if(works[i] > max)	max = works[i];
+            for(int i=0; i<works.length; i++) {
+                if(works[i] == max) {
+                    works[i] --;
+                    n --;
+                    if(n == 0)	break;
+                }
+            }
         }
-
-        for (int i = 0; i < works.length; i++) {
+        for(int i=0; i<works.length; i++) {
             answer += (works[i] * works[i]);
         }
         return answer;
