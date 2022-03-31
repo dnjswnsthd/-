88c88df98e52bf012b7967e13ed72d24445719e9
dnjswnsthd <dnjswnsthd@naver.com>
2022-03-31 13:55:02 +0900

    Add 프로그래머스 > 코딩테스트 연습 > 줄 서는 방법

---
+++ Algo/src/Programmers_level3/Pro_reLine.java
@@ -0,0 +1,26 @@
+import java.util.*;
+/*
+* 프로그래머스 > 코딩테스트 연습 > 줄 서는 방법
+* 22.03.31
+ */
+class Pro_reLine {
+    public int[] solution(int n, long k) {
+        int[] ret = new int[n];
+        List<Integer> list = new ArrayList<>();
+        long num=1;
+        int idx=0;
+        for(int x=1 ; x<=n ; x++) {
+            list.add(x);
+            num *= x;
+        }
+        k--;
+        while(idx<ret.length) {
+            num /= (n--);
+            int i = (int)(k/num);
+            ret[idx++] = list.get(i);
+            list.remove(i);
+            k %= num;
+        }
+        return ret;
+    }
+}
