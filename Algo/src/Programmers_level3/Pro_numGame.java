5171f7960a83a7ea259115c09435a0a3e019fdbf
dnjswnsthd <dnjswnsthd@naver.com>
2022-04-07 15:36:30 +0900

    Add 프로그래머스 > 코테 > sum/win > 숫자 게임

---
+++ Algo/src/Programmers_level3/Pro_numGame.java
@@ -0,0 +1,23 @@
+/*
+* 프로그래머스 > 코테 > sum/win > 숫자 게임
+* 22.04.07
+ */
+
+import java.util.*;
+
+class Pro_numGame {
+    public static final int INF = -1;
+    public int solution(int[] A, int[] B) {
+        int answer = 0;
+        Arrays.sort(A);
+        Arrays.sort(B);
+        int i_b = A.length-1;
+        for(int i_a=A.length-1; i_a >=0; i_a--){
+            if (B[i_b] > A[i_a]){
+                answer++;
+                i_b--;
+            }
+        }
+        return answer;
+    }
+}
