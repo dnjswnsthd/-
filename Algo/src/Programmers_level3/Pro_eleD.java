d5d14cb26f3e883bad2dfb009d082f6a7633aac9
dnjswnsthd <dnjswnsthd@naver.com>
2022-04-21 18:42:39 +0900

    Fix 기지국 설치 효율성 수정

--- Algo/src/Programmers_level3/Pro_eleD.java
+++ Algo/src/Programmers_level3/Pro_eleD.java
@@ -1,24 +1,19 @@
-import java.util.LinkedList;
-import java.util.Queue;
 /*
-* 프로그래머스 > 기지국 설치
-* 22.04.15
+ * 프로그래머스 > 기지국 설치
+ * 22.04.15 > 효율성 22.04.21
  */
 class Pro_eleD {
     public int solution(int n, int[] stations, int w) {
         int answer = 0;
-        Queue<Integer> sq = new LinkedList<Integer>();
-        for(int s : stations) {
-            sq.offer(s);
-        }
+        int si = 0;
         int position = 1;
-        while(position <= n) {
-            if(!sq.isEmpty() && position >= sq.peek() - w) {
-                position = sq.poll() + w + 1;
-            }
-            else {
+        while (position <= n) {
+            if (si < stations.length && position >= stations[si] - w) {
+                position = stations[si] + w + 1;
+                si++;
+            } else {
                 answer += 1;
-                position += (w*2) + 1;
+                position += (w * 2) + 1;
             }
         }
         return answer;
