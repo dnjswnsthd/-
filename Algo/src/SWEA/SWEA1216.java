7bb6056eae610805a1bdfd9ac2a10ca34c53cea7
dnjswnsthd <dnjswnsthd@naver.com>
2022-06-04 17:27:55 +0900

    fix swea1216

--- Algo/src/SWEA/SWEA1216.java
+++ Algo/src/SWEA/SWEA1216.java
@@ -12,33 +12,37 @@ public class SWEA1216 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		for(int t = 1; t <= 10; t++) {
+		for (int t = 1; t <= 10; t++) {
 			br.readLine();
 			int res = 0;
-			char [][] wordMap = new char[100][100];
-			for(int i = 0; i < 100; i++) {
+			char[][] wordMap = new char[100][100];
+			for (int i = 0; i < 100; i++) {
 				String s = br.readLine();
-				for(int j = 0; j < 100; j++) {
+				for (int j = 0; j < 100; j++) {
 					wordMap[i][j] = s.charAt(j);
 				}
 			}
-			for(int l = 0; l < 100; l++) {
+			for (int l = 0; l < 100; l++) {
 				for (int i = 0; i < 100; i++) {
 					for (int j = 0; j <= 100 - l; j++) {
 						int chk = 0;
 						for (int k = 0; k < l / 2; k++) {
-							if (wordMap[i][j + k] == wordMap[i][j + l - k - 1]) chk++;						
+							if (wordMap[i][j + k] == wordMap[i][j + l - k - 1])
+								chk++;
 						}
-						if (chk == l / 2) res = l;
+						if (chk == l / 2)
+							res = l;
 					}
 				}
-				for(int i = 0; i <= 100 - l; i++) {
-					for(int j = 0; j <100; j++) {
+				for (int i = 0; i <= 100 - l; i++) {
+					for (int j = 0; j < 100; j++) {
 						int chk = 0;
-						for(int k = 0; k < l / 2; k++) {
-							if(wordMap[i+k][j] == wordMap[i+l-k-1][j]) chk++;
+						for (int k = 0; k < l / 2; k++) {
+							if (wordMap[i + k][j] == wordMap[i + l - k - 1][j])
+								chk++;
 						}
-						if (chk == l / 2) res = l;
+						if (chk == l / 2)
+							res = l;
 					}
 				}
 			}
